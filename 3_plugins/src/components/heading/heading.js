69e3a06c2c9e9d60c87701d4f9c2c00a0fdc0513
Weslan Alves <weslan.alves@fandi.com.br>
2023-10-16 15:26:13 -0300

    feat(Plugins) Modulo de Plugins
    - Construindo módulo de plugins;

---
+++ 3_plugins/src/components/heading/heading.js
@@ -0,0 +1,14 @@
+import "./heading.css";
+
+class Heading {
+  create(title) {
+    const h1 = document.createElement("h1");
+
+    h1.innerText = title;
+    h1.classList.add("title");
+
+    document.querySelector("body").appendChild(h1);
+  }
+}
+
+export default Heading;
---
+++ 3_plugins/src/index.js
@@ -0,0 +1,8 @@
+import Heading from "./components/heading/heading.js";
+
+const heading = new Heading();
+
+heading.create("Página principal");
+
+console.log(VERSION, PORT);
+console.log(process.env.API_KEY);
